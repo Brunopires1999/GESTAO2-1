4e83d6da9243dd8bf9e0a018b56f7d71b8398f10
Brunopires1999 <123516656+Brunopires1999@users.noreply.github.com>
2023-03-20 20:13:20 -0300

    commit de alteração de código
    alteração total do codigo

---
+++ Configuracao2/DAL/Conexao.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DAL
+{
+    public static class Conexao
+    {
+        public static string StringDeConexao
+        {
+            get // funçao de apenas pegar dados e nao colocar
+            {
+                return @"User ID=SA;Initial Catalog=GESTAO2;Data Source=.\SQLEXPRESS2019;Password=Senailab02";
+            }
+        }
+    }
+}
